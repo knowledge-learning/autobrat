4720add051a955f665fdf2d75d93fdbc341dbe9f
Suilan Estévez <suilanestevez@gmail.com>
2019-02-21 13:38:48 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# BRAT docker for SAT+R annotation schema
+
+* Run `docker-compose build` to build the docker image.
+* Run `docker-compose up` and navigate to [http://localhost:8080](http://localhost:8080).
---
+++ build/brat_install_wrapper.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd /var/www/brat/brat-v1.3_Crunchy_Frog && /var/www/brat/brat-v1.3_Crunchy_Frog/install.sh <<EOD
+$BRAT_USERNAME
+$BRAT_PASSWORD
+$BRAT_EMAIL
+EOD
+
+chown -R www-data:www-data /bratdata
+
+# patch the user config with more users
+python /var/www/brat/brat-v1.3_Crunchy_Frog/user_patch.py
+
+echo "Install complete. You can log in as: $BRAT_USERNAME with password: $BRAT_PASSWORD."
+
+exit 0
